ec36f4380ea8f9efbeabf2dd555c8a7006c4d235
Tanner B. Hess Webber <tb@hesswebber.com>
2020-11-26 09:24:51 -0500

    build(eslint): updates base configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,72 +1,73 @@
+console.log(__dirname);
+
 module.exports = {
-  "root": true,
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module",
-    "project": "./tsconfig.base.json"
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.base.json',
+    tsconfigRootDir: __dirname,
   },
-  "ignorePatterns": ["**/*"],
-  "plugins": ["@typescript-eslint", "@nrwl/nx"],
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    // "plugin:@typescript-eslint/recommended-requiring-type-checking", 
-    "prettier",
-    "prettier/@typescript-eslint"
+  ignorePatterns: ['**/*'],
+  plugins: ['@typescript-eslint', '@nrwl/nx'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    // "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    'prettier',
+    'prettier/@typescript-eslint',
   ],
-  "rules": {
-    "@typescript-eslint/explicit-member-accessibility": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-parameter-properties": "off",
-    "@nrwl/nx/enforce-module-boundaries": [
-      "error",
+  rules: {
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-parameter-properties': 'off',
+    '@nrwl/nx/enforce-module-boundaries': [
+      'error',
       {
-        "enforceBuildableLibDependency": true,
-        "allow": [],
-        "depConstraints": [
-          { "sourceTag": "*", "onlyDependOnLibsWithTags": ["*"] }
-        ]
-      }
-    ]
+        enforceBuildableLibDependency: true,
+        allow: [],
+        depConstraints: [{ sourceTag: '*', onlyDependOnLibsWithTags: ['*'] }],
+      },
+    ],
   },
-  "overrides": [
+  overrides: [
     {
-      "files": ["*.ts", "*.tsx", "*.js", "*.jsx"],
-      "rules": {
-        "@nrwl/nx/enforce-module-boundaries": [
-          "error",
+      files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
+      rules: {
+        '@nrwl/nx/enforce-module-boundaries': [
+          'error',
           {
-            "enforceBuildableLibDependency": true,
-            "allow": [],
-            "depConstraints": [
+            enforceBuildableLibDependency: true,
+            allow: [],
+            depConstraints: [
               {
-                "sourceTag": "*",
-                "onlyDependOnLibsWithTags": ["*"]
-              }
-            ]
-          }
-        ]
-      }
+                sourceTag: '*',
+                onlyDependOnLibsWithTags: ['*'],
+              },
+            ],
+          },
+        ],
+      },
     },
     {
-      "files": ["*.ts", "*.tsx"],
-      "extends": ["plugin:@nrwl/nx/typescript"],
-      "parserOptions": {
-        "project": "./**/tsconfig.?*.json",
-        "tsconfigRootDir": __dirname
+      files: ['*.ts', '*.tsx'],
+      extends: ['plugin:@nrwl/nx/typescript'],
+      parserOptions: {
+        project: './**/tsconfig.?*.json',
+        tsconfigRootDir: __dirname,
       },
-      "rules": {}
+      rules: {},
     },
     {
-      "files": ["*.js", "*.jsx"],
-      "extends": ["plugin:@nrwl/nx/javascript"],
-      "parserOptions": {
-        "project": "./**/tsconfig.?*.json",
-        "tsconfigRootDir": __dirname
+      files: ['*.js', '*.jsx'],
+      extends: ['plugin:@nrwl/nx/javascript'],
+      parserOptions: {
+        project: './**/tsconfig.?*.json',
+        tsconfigRootDir: __dirname,
       },
-      "rules": {}
-    }
-  ]
-}
+      rules: {},
+    },
+  ],
+};
